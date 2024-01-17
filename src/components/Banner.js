71ce7f8738c39d4b56e41cc90a95516abdd22d77
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2024-01-17 14:01:57 +0530

    changed cv link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,7 +58,7 @@ export const Banner = () => {
               <h2 className="txt-rotate"><span className="wrap">{text}</span></h2>
               <p>A skilled Full Stack Developer with 2 years of hands-on experience, dedicated to creating seamless and
                 responsive user interfaces. Proficient in building dynamic web applications, I bring expertise in JavaScript, React, and modern front-end technologies. Passionate about crafting intuitive user experiences, I thrive in collaborative environments, delivering high-quality solutions tailored to user needs.</p>
-              <a href='https://drive.google.com/file/d/10Rz51kb9t57sRxmnq8DkX_iW46r1MGMN/view?usp=share_link' target="_blank">
+              <a href='https://drive.google.com/file/d/1F1XQG6ptPKeLY6-GbmZa5Xhu9GU8_4Wg/view?usp=share_link' target="_blank">
                 <button>Download CV <ArrowRightCircle size={25} /></button>
               </a>
             </div>
